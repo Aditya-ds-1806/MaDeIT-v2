fe2e2e6940ad2d55bd827bf73445965c279c4dc6
Aditya D.S <adithya18062000@gmail.com>
2020-07-17 23:20:45 +0530

    file upload support

---
+++ public/uploadFile.php
@@ -0,0 +1,38 @@
+<?php
+define('SITE_ROOT', realpath(dirname(__FILE__)));
+$target_dir = $_POST['target-dir'];
+$target_file = SITE_ROOT . $target_dir . basename($_FILES["file"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["file"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo " Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+if ($_FILES["file"]["size"] > 2000000) { // > 2MB
+    echo " Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+    echo " Sorry, your file was not uploaded.";
+} else {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        echo " The file " . basename($_FILES["file"]["name"]) . " has been uploaded.";
+    } else {
+        echo " Sorry, there was an error uploading your file. Either the file size was too large (>2MB) or poor internet.";
+    }
+}
--- views/update/tenders.php
+++ views/update/tenders.php
@@ -13,19 +13,68 @@
 <body class="p-2">
     <div class="container text-center">
         <h1>Update Tenders</h1>
+        <div id="message" class="alert alert-info fixed-top font-weight-bold d-none" role="alert">
+        </div>
         <div class="alert alert-danger d-none fixed-top font-weight-bold" role="alert" id="error">
             Please correct errors before submitting!
         </div>
         <div class="alert alert-success d-none fixed-top font-weight-bold" role="alert" id="success">
             All changes have been updated succesfully!
         </div>
-        <div class="alert alert-danger d-none fixed-top font-weight-bold" role="alert" id="error">
+        <div class="alert alert-danger d-none fixed-top font-weight-bold" role="alert" id="fail">
             Something went wrong, changes could not be saved. Please try again in some time.
         </div>
+        <form id="form" class="text-left my-5">
+            <div class="input-group">
+                <div class="custom-file">
+                    <input type="file" class="custom-file-input" accept=".pdf" name="file" id="inputGroupFile02" onchange="$(this).next('.custom-file-label').html($(this).val()); $('#name').val($(this).val())" required>
+                    <label id="fileName" class="custom-file-label" for="inputGroupFile02" aria-describedby="inputGroupFileAddon02">Upload Tender document</label>
+                </div>
+                <div class="input-group-append">
+                    <button id="upload" type="submit" class="btn btn-primary">
+                        Upload
+                        <span class="spinner-border d-none" id="upload-spinner" role="status" style="width:1em; height:1em"></span>
+                    </button>
+                </div>
+            </div>
+            <small class="text-muted">Please note that the maximum file size is 2MB.</small>
+        </form>
         <div class="text-left my-4" id="jsoneditor" style="height: 75vh"></div>
-        <button id="submit" class="btn btn-lg btn-primary">Save and Update!</button>
+        <button id="submit" class="btn btn-lg btn-primary mb-5">
+            <span>Save and Update!</span>
+            <span class="spinner-border d-none" id="saveSpinner" role="status" style="width:1em; height:1em"></span>
+        </button>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script>
+        $("#form").on('submit', function(e) {
+            e.preventDefault();
+            $("#upload-spinner").removeClass('d-none');
+            var form = $('#form')[0];
+            var formData = new FormData(form);
+            formData.append("target-dir", "/tenders/");
+            $.ajax({
+                type: "POST",
+                url: "public/uploadFile.php",
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function(res) {
+                    $("#message").text(res);
+                    $("#message").removeClass('d-none');
+                    $("#upload-spinner").addClass('d-none');
+                    setTimeout(function() {
+                        $("#message").addClass('d-none');
+                    }, 4000)
+                },
+                error: function() {
+                    $('#upload-spinner').removeClass('d-none');
+                    document.querySelector("#message").classList.remove('d-none');
+                    document.querySelector("#message").textContent = "Request timeout, check your internet connection";
+                }
+            });
+        });
+    </script>
     <script>
         function hasSchemaOrParseErrors() {
             var errorCls = ["jsoneditor-validation-error-icon", "jsoneditor-schema-error"];
@@ -101,11 +150,12 @@
             editor.set(tenders);
             editor.expandAll();
             document.querySelector('button#submit').addEventListener('click', () => {
+                $('#saveSpinner').removeClass('d-none');
                 if (hasSchemaOrParseErrors()) {
                     document.querySelector("#error").classList.remove('d-none');
                     setTimeout(function() {
                         document.querySelector("#error").classList.add('d-none');
-                    }, 2000)
+                    }, 4000)
                 } else {
                     var newJson = editor.get();
                     $.ajax({
@@ -118,17 +168,26 @@
                         },
                         cache: false,
                         success: function(res) {
+                            $('#saveSpinner').addClass('d-none');
                             if (res) {
                                 document.querySelector("#success").classList.remove('d-none');
                                 setTimeout(function() {
                                     document.querySelector("#success").classList.add('d-none');
-                                }, 2000);
+                                }, 4000);
                             } else {
-                                document.querySelector("#error").classList.remove('d-none');
+                                document.querySelector("#fail").classList.remove('d-none');
                                 setTimeout(function() {
-                                    document.querySelector("#error").classList.add('d-none');
-                                }, 2000);
+                                    document.querySelector("#fail").classList.add('d-none');
+                                }, 4000);
                             }
+                        },
+                        error: function() {
+                            $('#saveSpinner').addClass('d-none');
+                            document.querySelector("#message").classList.add('d-none');
+                            document.querySelector("#message").textContent = "Request timeout, check your internet connection";
+                            setTimeout(function() {
+                                document.querySelector("#message").classList.add('d-none');
+                            }, 4000);
                         }
                     });
                 }
