dda4059c3639661d3a9d34376c7d6c59c61541e8
Aditya D.S <adithya18062000@gmail.com>
2020-07-27 01:19:51 +0530

    programs admin section updated

--- views/partials/program-cards.php
+++ views/partials/program-cards.php
@@ -1,7 +1,7 @@
 <div class="card-group scrolling-wrapper">
     <?php for ($i = 0; $i < count($programs); $i++) { ?>
         <div class="card shadow-sm bg-white text-white w-100 w-sm-50 w-lg-33 border-0 <?php if ($i === 0) { ?>mr-4<?php } elseif ($i === count($programs) - 1) { ?>ml-4<?php } else { ?>mx-4<?php } ?>">
-            <img loading="lazy" src="<?php echo $programs[$i]['image'] ?>" class="card-img-top border-0 m-0" alt="<?php echo $programs[$i]['program'] ?>">
+            <img loading="lazy" src="<?php echo "public/images/programmes/" . $programs[$i]['fileName'] ?>" class="card-img-top border-0 m-0" alt="<?php echo $programs[$i]['program'] ?>">
             <div class="card-body rounded text-dark">
                 <h5 class="card-title"><?php echo $programs[$i]['program'] ?></h5>
                 <p class="card-text"><?php echo $programs[$i]['description'] ?></p>
